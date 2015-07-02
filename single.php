3ea4faf2eca1694dc287b7ae27a9337e728c39a8
Joshua Adela <joshuaadela@gmail.com>
2015-07-02 17:25:20 +0800

    Added Post Page with Thumbnail Ticket: #39993510355268

--- single.php
+++ single.php
@@ -1,25 +1,46 @@
-<div class="container-fluid">
-  <div class="row">
-  </div>
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="row">
-			<div class="col-lg-8">
-				<h1><?php the_title(); ?></h1>
-				<h4>Posted on <?php the_time('F jS, Y') ?></h4>
-				<p><?php the_content(__('(more...)')); ?></p>
+<?php get_header(); ?>
+	<div class="container container-white">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12 ">
+					<div class="row text-center">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<h1 class="text-uppercase"><strong><?php the_title(); ?></strong></h1>
+						</div>
+					</div>
+					<hr/>
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+							<div class="pull-left post-thumbnail">
+							<?php
+							if ( has_post_thumbnail() ) {
+								the_post_thumbnail();
+							}
+							?>
+							</div>
+							<p><?php the_content(__('(more...)')); ?></p>
+							<p>by <?php the_author() ?></p>
+						</div>
+						<div class="hidden-lg hidden-md hidden-sm">
+							<div class="post-thumbnail">
+								<center>
+									<?php
+									if ( has_post_thumbnail() ) {
+										the_post_thumbnail('full', array( 'class' => 'img-responsive col-xs-12' ));
+									}
+									?>
+								</center>
+							</div>
+						</div>
+						<div class="hidden-lg hidden-md hidden-sm col-xs-12">
+							<p><?php the_content(__('(more...)')); ?></p>
+							<p>by <?php the_author() ?></p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-lg-4">
-				<?php get_sidebar(); ?>
-			</div>
-		</div>
-	<?php endwhile; else: ?>
-		<div class="row">
-			<div class="col-lg-8">
-				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-			</div>
-			<div class="col-lg-4">
-				<?php get_sidebar(); ?>
-			</div>
-		</div>
-	<?php endif; ?>
-</div>
+		<?php endwhile; else: ?>
+
+		<?php endif; ?>
+	</div>
+<?php get_footer(); ?>
