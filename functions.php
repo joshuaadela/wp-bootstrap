d6b2136c0c9d320061012ba1da9a2b8244c1f74d
Joshua Adela <joshuaadela@gmail.com>
2015-07-02 18:27:48 +0800

    Add Post Templates based from Category. Add post podcast template. Ticket: #37364849509658

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php 
+
 	add_theme_support( 'post-thumbnails'); 
 	set_post_thumbnail_size( 400, 400 );
+
+	//Post Template depending on category
+	//Gets post cat slug and looks for single-[cat slug].php and applies it
+	add_filter('single_template', create_function(
+		'$the_template',
+		'foreach( (array) get_the_category() as $cat ) {
+			if ( file_exists(TEMPLATEPATH . "/single-{$cat->slug}.php") )
+			return TEMPLATEPATH . "/single-{$cat->slug}.php"; }
+		return $the_template;' )
+	);
---
+++ single-podcast.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Single Post Template: Podcast
+*/
+?>
+<?php get_header(); ?>
+	<div class="container container-white">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12 ">
+					<div class="row text-center">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<h1 class="text-uppercase"><strong><?php the_title(); ?></strong></h1>
+						</div>
+					</div>
+					<hr/>
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+							<div class="pull-left post-thumbnail">
+							<?php
+							if ( has_post_thumbnail() ) {
+								the_post_thumbnail();
+							}
+							?>
+							</div>
+							<p><?php the_content(__('(more...)')); ?></p>
+						</div>
+						<div class="hidden-lg hidden-md hidden-sm">
+							<div class="post-thumbnail">
+								<center>
+									<?php
+									if ( has_post_thumbnail() ) {
+										the_post_thumbnail('full', array( 'class' => 'img-responsive col-xs-12' ));
+									}
+									?>
+								</center>
+							</div>
+						</div>
+						<div class="hidden-lg hidden-md hidden-sm col-xs-12">
+							<p><?php the_content(__('(more...)')); ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; else: ?>
+
+		<?php endif; ?>
+	</div>
+<?php get_footer(); ?>
