d068452ec36f10fb3f819c94a451589b13d7c374
Joshua Adela <joshuaadela@gmail.com>
2015-07-04 02:01:17 +0800

    Updated Podcast List View Ticket: #37364849509658

--- page-podcast.php
+++ page-podcast.php
@@ -20,19 +20,54 @@ Template Name: Podcast Page
 						$wp_query = NULL;
 						$wp_query = new WP_Query();
 						$wp_query->query('cat=9&paged='.$paged);
-						$tb_counter = 1;
+						$tb_counter = 0;
 						?>
 						<?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-							<div class="row">
-								<a href="<?php the_permalink(); ?>">
-									<?php
-									if ( has_post_thumbnail() ) {
-										the_post_thumbnail('full', array( 'class' => 'img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12' ));
-									}
-									?>
-								</a>
-							</div>
-							<br/>
+							<?php if ($tb_counter == 0): ?>
+								<div class="row">
+									<a href="<?php the_permalink(); ?>">
+										<?php
+										if ( has_post_thumbnail() ) {
+											the_post_thumbnail('full', array( 'class' => 'img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12' ));
+										}
+										?>
+									</a>
+								</div>
+								<br/>
+							<?php else: ?>
+								<div class="hidden-sm hidden-xs">
+									<div class="row">
+										<a href="<?php the_permalink(); ?>">
+											<?php
+											if ( has_post_thumbnail() ) {
+												the_post_thumbnail('full', array( 'class' => 'img-responsive col-md-3 col-lg-3' ));
+											}
+											?>
+										</a>
+										<?php if(($tb_counter-1)%4 == 0): ?>
+											</div>
+											<br/>
+											<div class="row">
+										<?php endif; ?>
+									</div>
+								</div>
+								<div class="hidden-md hidden-lg">
+									<div class="row">
+										<a href="<?php the_permalink(); ?>">
+											<?php
+											if ( has_post_thumbnail() ) {
+												the_post_thumbnail('full', array( 'class' => 'img-responsive col-xs-6 col-sm-6' ));
+											}
+											?>
+										</a>
+										<?php if(($tb_counter-1)%2 == 0): ?>
+											</div>
+											<br/>
+											<div class="row">
+										<?php endif; ?>
+									</div>
+								</div>
+							<?php endif; ?>
 						<?php $tb_counter++; endwhile; ?>
 						<?php $wp_query = NULL; $wp_query = $temp;?>
 					</div>
